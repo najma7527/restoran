9773e6665227956779ca20aeb89d9ba4b4f111f4
najma7572 <salmanajma072@gmail.com>
2025-05-18 21:31:42 +0700

    pembenaan crud m bayar, diskon, metode, dan stok

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::get('/login', function () {
     return view('login');
 })->name('login.form');
 
-Route::post('/login', [LoginController::class, 'login'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 
